54261f1cad0e29ed019f244977c30245db6d7377
Stephen Blum <stephen@pubnub.com>
2016-10-15 20:36:03 -0700

    finished first rev on BLOCKS first design.

--- handlers/reactions.js
+++ handlers/reactions.js
@@ -1,4 +1,13 @@
-// reactions to signals
-opengrowth.rections( ( signal, customer ) = {
-    console.log( signal, customer );
-} );
+// Reactions Handler
+export default request => {
+    const message = request.message;
+    const signal  = message.signal;
+    const email   = message.email;
+
+    // TODO Get Clearbit/MonkeyLearn
+    // TODO from KV or build from scratch.
+    opengrowth.customer(email).then( customer => {
+        console.log( email, customer, signal );
+    } );
+}
+
