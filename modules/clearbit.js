d0d344c98c7d8b7f07211373ad6b27c19cc7ab25
Stephen Blum <stephen@pubnub.com>
2016-11-01 20:17:19 -0700

    added modules.

---
+++ modules/clearbit.js
@@ -0,0 +1,11 @@
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Lookup Customers with Clearbit
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+opengrowth.modules.clearbit = {};
+opengrowth.modules.clearbit.lookup = (email) => {
+    // ⚠️  opengrowth.keys.clearbit.apikey
+    // TODO return promise
+    // TODO return promise
+    // TODO return promise
+};
+
---
+++ modules/monkeylearn.js
@@ -0,0 +1,12 @@
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Leverage a Classifier with MonekyLearn
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+opengrowth.modules.monkeylearn = {};
+opengrowth.modules.monkeylearn.lookup = ( input, classifier ) => {
+    // ⚠️  opengrowth.keys.monkeylearn.apikey
+    // TODO return promise
+    // TODO return promise
+    // TODO return promise
+};
+
+
