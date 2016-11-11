5d6fbbf81988f1730d12a5efb9d98c038b8a511f
Stephen Blum <stephen@pubnub.com>
2016-11-10 21:06:33 -0800

    added the config import for Heroku env vars.

--- generators/config.js
+++ generators/config.js
@@ -2,7 +2,7 @@
 // Config Export
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 module.exports = {
-    //pubkey : process.env.pubkey
-//,   subkey : process.env.subkey
-//,   seckey : process.env.seckey
+    pubkey : process.env.PUBKEY
+,   subkey : process.env.SUBKEY
+,   seckey : process.env.SECKEY
 };
