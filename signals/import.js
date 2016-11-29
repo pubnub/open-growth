74b4680021d461c012795381b7d1a9eda4bfc28a
Dustin Raimondi <raimondi1337@gmail.com>
2016-11-29 15:07:42 -0800

    added KV store import signal

--- signals/import.js
+++ signals/import.js
@@ -1,4 +1,19 @@
+//import data into the KV store
+//expected message format:
+//{"companyName": "email@address.com"}
 opengrowth.signals.import = (request) => {
-    kvdb.jaslkdfjls
+    for (var key in request.message) {
+        kvdb.get(key.toLowerCase().replace(/[^0-9a-z]/gi, '')).then(val => {
+	    if (val) {
+	        if (val.indexOf(request.message[key]) == -1) {//.includes not supported yet??
+		    val.push(request.message[key]);
+		    kvdb.set(key.toLowerCase().replace(/[^0-9a-z]/gi, ''), val);
+		}
+            } else {
+                kvdb.set(key.toLowerCase().replace(/[^0-9a-z]/gi, ''), [request.message[key]]);
+            }
+        });
+    }
+
     return request.ok();
 };
