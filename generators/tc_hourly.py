1d039a97667c3bd2fa716c1ed97a75b8f1a64863
Adam Bavosa <adamb@adam-bavosa.local>
2016-12-01 12:19:51 -0800

    tc_scrape now runs hourly

--- generators/tc_hourly.py
+++ generators/tc_hourly.py
@@ -5,7 +5,6 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 
 # Run tc_scrape.main every hour
-#sched.add_job(tc_scrape.main, 'interval', hours=1)
-sched.add_job(tc_scrape.main, 'interval', minutes=1)
+sched.add_job(tc_scrape.main, 'interval', hours=1)
 
 sched.start()
