2741c351907754059af34dc82f65cdef3c115b4f
Dustin Raimondi <raimondi1337@gmail.com>
2016-12-01 13:52:09 -0800

    changed data structure

---
+++ signals/techcrunch.js
@@ -0,0 +1,17 @@
+opengrowth.signals.techcrunch = ( request ) => {
+    const recipient = 'open-growth-activity@pubnub.com';
+    const subject = 'Techcrunch Signal';
+    const message = '';
+    const keywords = request.message.keyword_extraction;
+    
+    for (item in keywords){
+	kvdb.get(item.keyword.toLowerCase().replace(/[^0-9a-z]/gi, '')).then(val => {
+            if (val) {
+	        message = "These guys were in the news: " + companyName + "\n\nWe should email: " + val;
+	        opengrowth.delight.sendgrid.email(
+	            'techcrunch', message, recipient, companyName, subject
+                );
+            }
+        }
+    });
+};
