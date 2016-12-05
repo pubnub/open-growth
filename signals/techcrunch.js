9170341ad7aac01dcc3984a80067cb612bdf9ce1
Dustin Raimondi <raimondi1337@gmail.com>
2016-12-05 14:43:33 -0800

    added deduplication of techcrunch signals

--- signals/techcrunch.js
+++ signals/techcrunch.js
@@ -3,19 +3,25 @@ opengrowth.signals.techcrunch = ( request ) => {
     const subject = 'Techcrunch Signal';
     const keywords = request.message.payload.keyword_extraction;
     const url = request.message.payload.url;
+    const deDupeID = request.message.payload.classification.category_id;
 
-    keywords.forEach(function(item){
-        let companyName = item.keyword;
-        let kw = item.keyword.toLowerCase().replace(/[^0-9a-z]/gi, '');
-        kvdb.get(kw).then(value => {
-            if(value){
-                var message ="<p>These guys were in the news: " + companyName + "</p>"
-                + "<p>In article: " +  url + "</p>"
-                + "<p>We should email: " + value + "</p>";
-                opengrowth.delight.sendgrid.email(
-                    'techcrunch', message, recipient, companyName, subject
-                );
-            }
-        });
+    kvdb.get(deDupeID).then(duplicate => {
+        if(!duplicate){
+            kvdb.set(deDupeID, true);
+            keywords.forEach(function(item){
+                let companyName = item.keyword;
+                let kw = item.keyword.toLowerCase().replace(/[^0-9a-z]/gi, '');
+                kvdb.get(kw).then(value => {
+                    if(value){
+                        var message ="<p>These guys were in the news: " + companyName + "</p>"
+                        + "<p>In article: " +  url + "</p>"
+                        + "<p>We should email: " + value + "</p>";
+                        opengrowth.delight.sendgrid.email(
+                            'techcrunch', message, recipient, companyName, subject
+                        );
+                    }
+                });
+            });
+        }
     });
 };
