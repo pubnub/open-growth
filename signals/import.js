e284a257712a68d62ecba246e43cf883db0811fc
Dustin Raimondi <raimondi1337@gmail.com>
2016-12-05 14:24:35 -0800

    fixed bugs in techcrunch signal and changed datastructure

--- signals/import.js
+++ signals/import.js
@@ -3,17 +3,18 @@
 //{"companyName": "email@address.com"}
 opengrowth.signals.import = (request) => {
     for (var key in request.message) {
-        kvdb.get(key.toLowerCase().replace(/[^0-9a-z]/gi, '')).then(val => {
-	    if (val) {
-	        if (val.indexOf(request.message[key]) == -1) {//.includes not supported yet??
-		    val.push(request.message[key]);
-		    kvdb.set(key.toLowerCase().replace(/[^0-9a-z]/gi, ''), val);
-		}
-            } else {
-                kvdb.set(key.toLowerCase().replace(/[^0-9a-z]/gi, ''), [request.message[key]]);
-            }
-        });
+        if(key!="signal"){
+            kvdb.get(key.toLowerCase().replace(/[^0-9a-z]/gi, '')).then(val => {
+                if (val) {
+                    if (val.indexOf(request.message[key]) == -1) { //.includes not supported yet??
+                        val.push(request.message[key]);
+                        kvdb.set(key.toLowerCase().replace(/[^0-9a-z]/gi, ''), val);
+                    }
+                } else {
+                    kvdb.set(key.toLowerCase().replace(/[^0-9a-z]/gi, ''), [request.message[key]]);
+                }
+            });
+        }
     }
-
     return request.ok();
 };
--- signals/techcrunch.js
+++ signals/techcrunch.js
@@ -2,16 +2,21 @@ opengrowth.signals.techcrunch = ( request ) => {
     const recipient = 'open-growth-activity@pubnub.com';
     const subject = 'Techcrunch Signal';
     const message = '';
-    const keywords = request.message.keyword_extraction;
-    
-    for (var item in keywords){
-	kvdb.get(item.keyword.toLowerCase().replace(/[^0-9a-z]/gi, '')).then(val => {
-            if (val) {
-	        message = "These guys were in the news: " + companyName + "\n\nWe should email: " + val;
-	        opengrowth.delight.sendgrid.email(
-	            'techcrunch', message, recipient, companyName, subject
+    const keywords = request.message.payload.keyword_extraction;
+    const url = request.message.payload.url;
+
+    keywords.forEach(function(item){
+        let companyName = item.keyword;
+        let kw = item.keyword.toLowerCase().replace(/[^0-9a-z]/gi, '');
+        kvdb.get(kw).then(value => {
+            if(value){
+                var message ="<p>These guys were in the news: " + companyName + "</p>"
+                + "<p>In article: " +  url + "</p>"
+                + "<p>We should email: " + value + "</p>";
+                opengrowth.delight.sendgrid.email(
+                    'techcrunch', message, recipient, companyName, subject
                 );
             }
         });
-    }
+    });
 };
