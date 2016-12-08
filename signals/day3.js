55bd53561235d9667f0480f199a32f9d43636664
Dustin Raimondi <raimondi1337@gmail.com>
2016-12-08 13:50:45 -0800

    added full email templates to all drip signals - string conversions are not currently hooked up to some things

--- signals/day3.js
+++ signals/day3.js
@@ -1,16 +1,22 @@
 opengrowth.signals.day3 = ( request, customer ) => {
-    const name = '';
+    const email = 'open-growth-activity@pubnub.com';//request.message.email
+    const subject = `${customer.company.name} - adding realtime to your app`;
 
-    const message = 'this gets sent to everyone that signed up 3 days ago';
+    let name = '';
+    try       { name = customer.person.name.fullName }
+    catch (e) { name = 'there' }
 
-    // Debug show Message
-    console.log(message);
+    var fname = '';
+    try       { fname = ', '+customer.person.name.givenName + '.' }
+    catch (e) { fname = '.' }
 
-    // Send Email
-    //const email   = request.message.email;
-    //const email   = 'blum.stephen@gmail.com';
-    const email   = 'open-growth-activity@pubnub.com';
-    const subject = 'day3';
+    const message =
+        `<p>Hope you’ve had the time to navigate through the PubNub Admin Portal and the PubNub Docs Page over the last few days${fname}</p>` + //this might get weird
+        `<p>On another note, I see that you work at ${customer.company.name}. Curious if you or another member of ${customer.company.name} is assessing PubNub to power realtime data transfer in your application?</p>` + 
+        `<p>Looking forward to hearing from you soon. In the meantime, here's a developer's guide to PubNub:</p>` + 
+        `<p><a href="https://www.pubnub.com/developers/tech/how-pubnub-works/">How PubNub Works</a></p>` +
+        `<p>Best, ${customer.csm.name}</p>`;
+
     opengrowth.delight.sendgrid.email(
         'day3', message, email, name, subject
     );
--- signals/day7.js
+++ signals/day7.js
@@ -1,16 +1,17 @@
 opengrowth.signals.day7 = ( request, customer ) => {
-    const name = '';
+    const email = 'open-growth-activity@pubnub.com';//request.message.email
+    const subject = `Following up re: PubNub`;
 
-    const message = 'this gets sent to everyone that signed up 7 days ago';
+    let name = '';
+    try       { name = customer.person.name.fullName }
+    catch (e) { name = 'there' }
 
-    // Debug show Message
-    console.log(message);
+    const message =
+        `<p>${name} - I wanted to follow up on my email in case it got buried the other day.</p>` + //this might get weird
+        `<p>Do you or someone from the ${customer.company.name} engineering team need help setting up realtime functionality with PubNub?</p>` + 
+        `<p>PS: Check out our new "Introduction to PubNub BLOCKS" developer training video <a href="https://www.pubnub.com/developers/tech/how-pubnub-works/">here</a>.</p>` +
+        `<p>Best, ${customer.csm.name}</p>`;
 
-    // Send Email
-    //const email   = request.message.email;
-    //const email   = 'blum.stephen@gmail.com';
-    const email   = 'open-growth-activity@pubnub.com';
-    const subject = 'day7';
     opengrowth.delight.sendgrid.email(
         'day7', message, email, name, subject
     );
