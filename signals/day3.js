c13b035504322513b4002e37a593dd3ffd0758a5
Dustin Raimondi <raimondi1337@gmail.com>
2016-12-13 13:59:09 -0800

    fixed csm name bug

--- signals/day3.js
+++ signals/day3.js
@@ -1,10 +1,12 @@
 opengrowth.signals.day3 = ( request, customer ) => {
     const email = 'open-growth-activity@pubnub.com';//request.message.email
-    const subject = `${customer.company.name} - adding realtime to your app`;
+    let subject = '';
+    try      { subject = `${customer.company.name} - adding realtime to your app`}
+    catch(e) { subject = `Adding realtime to your app`}
 
     let name = '';
     try       { name = customer.person.name.fullName }
-    catch (e) { name = 'there' }
+    catch (e) { name = '' }
 
     var fname = '';
     try       { fname = ', '+customer.person.name.givenName + '.' }
@@ -12,10 +14,10 @@ opengrowth.signals.day3 = ( request, customer ) => {
 
     const message =
         `<p>Hope you’ve had the time to navigate through the PubNub Admin Portal and the PubNub Docs Page over the last few days${fname}</p>` + //this might get weird
-        `<p>On another note, I see that you work at ${request.company.name}. Curious if you or another member of ${request.company.name} is assessing PubNub to power realtime capabilities in your application?</p>` + 
+        `<p>On another note, I see that you work at ${customer.company.name}. Curious if you or another member of ${customer.company.name} is assessing PubNub to power realtime capabilities in your application?</p>` + 
         `<p>Looking forward to hearing from you soon. In the meantime, here's a developer's guide to PubNub:</p>` + 
         `<p><a href="https://www.pubnub.com/developers/tech/how-pubnub-works/?utm_source=EmailBlasts%20&utm_medium=Open-Growth&utm_campaign=EB-CY16-Q4-Open-Growth-02&utm_term=link1&utm_content=how-pubnub-works">How PubNub Works</a></p>` +
-        `<p>Best, ${request.csm.name}</p>`;
+        `<p>Best, ${request.message.csm.first_name}</p>`;
 
     opengrowth.delight.sendgrid.email(
         'day3', message, email, name, subject
