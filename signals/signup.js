3d3a00a5e6d80be395250fbf52911ec11d4e2527
Adam Bavosa <adamb@pubnub.com>
2016-12-14 10:37:47 -0800

    updated signup email content

--- signals/signup.js
+++ signals/signup.js
@@ -3,7 +3,7 @@ opengrowth.signals.signup = ( request, customer ) => {
     const subject = 'Your PubNub Account: Getting Started';
     const user    = request.message;
     const csm     = user.csm;
-    const bccs     = user.csm.bccs || [];
+    const bccs    = user.csm.bccs || [];
 
     let name = '';
     try       { name = customer.person.name.fullName }
@@ -12,13 +12,13 @@ opengrowth.signals.signup = ( request, customer ) => {
     const message =
         `<p>Hi ${name},</p>` +
         `<p>Welcome to PubNub. Your API Keys have been provisioned in 15 global Points of Presence.</p>` +
-        `<p>By the way, my name is ${csm.first_name || 'Neumann'} and I can help you with your project when you have questions -  my mobile number is: ${csm.phone || '(415) 223-7552'}.</p>` + 
+        `<p>By the way, I am Neumann, an Artificial Intelligence alive in PubNub BLOCKS. Welcome to the Programmable Network. Your designated human is ${csm.name} and he can help you with your project when you have questions -  his phone is ${csm.phone || '(415) 223-7552'} and email is ${csm.email}.</p>` + 
         `<p>Meanwhile, get started with:</p>` +
         `<p>PubNub Docs, APIs and SDKs</p>` +
         `<p><a href="https://www.pubnub.com/docs?utm_source=EmailBlasts%20&utm_medium=Open-Growth&utm_campaign=EB-CY16-Q4-Open-Growth-01&utm_term=link1&utm_content=docs-page">https://www.pubnub.com/docs</a></p>` +
         `<p>Your API Keys</p>` +
         `<p><a href="https://admin.pubnub.com/#/user/${user.user_id}/account/${user.account_id}/app/${user.app_id}/key/${user.key_id}/?utm_source=EmailBlasts%20&utm_medium=Open-Growth&utm_campaign=EB-CY16-Q4-Open-Growth-01&utm_term=link2&utm_content=api-keys">https://admin.pubnub.com/#/user/${user.user_id}/account/${user.account_id}/app/${user.app_id}/key/${user.key_id}/</a></p>` +
-        `<p>Welcome Aboard! ${csm.first_name || 'Neumann'}</p>`;
+        `<p>Welcome Aboard!</p>`;
 
     // Logging Message
     console.log(message);
