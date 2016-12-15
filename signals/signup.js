e351d68150179d336d530ea9900e9178db19fb15
Adam Bavosa <adamb@pubnub.com>
2016-12-15 10:59:59 -0800

    adjusted signup email script

--- signals/signup.js
+++ signals/signup.js
@@ -12,12 +12,14 @@ opengrowth.signals.signup = ( request, customer ) => {
     const message =
         `<p>Hi ${name},</p>` +
         `<p>Welcome to PubNub. Your API Keys have been provisioned in 15 global Points of Presence.</p>` +
-        `<p>By the way, I am Neumann, an Artificial Intelligence alive in PubNub BLOCKS. Welcome to the Programmable Network. Your designated human is ${csm.name} and he can help you with your project when you have questions -  his phone is ${csm.phone || '(415) 223-7552'} and email is ${csm.email}.</p>` + 
+        `<p>By the way, I am Neumann, an Artificial Intelligence alive in PubNub BLOCKS. Welcome to the Programmable Network. Your designated human is ${csm.full_name} and he can help you with your project when you have questions -  his phone is ${csm.phone || '(415) 223-7552'} and email is ${csm.email}.</p>` + 
         `<p>Meanwhile, get started with:</p>` +
-        `<p>PubNub Docs, APIs and SDKs</p>` +
-        `<p><a href="https://www.pubnub.com/docs?utm_source=EmailBlasts%20&utm_medium=Open-Growth&utm_campaign=EB-CY16-Q4-Open-Growth-01&utm_term=link1&utm_content=docs-page">https://www.pubnub.com/docs</a></p>` +
-        `<p>Your API Keys</p>` +
-        `<p><a href="https://admin.pubnub.com/#/user/${user.user_id}/account/${user.account_id}/app/${user.app_id}/key/${user.key_id}/?utm_source=EmailBlasts%20&utm_medium=Open-Growth&utm_campaign=EB-CY16-Q4-Open-Growth-01&utm_term=link2&utm_content=api-keys">https://admin.pubnub.com/#/user/${user.user_id}/account/${user.account_id}/app/${user.app_id}/key/${user.key_id}/</a></p>` +
+        `<p>PubNub Docs, APIs and SDKs` +
+        `<br />`+
+        `<a href="https://www.pubnub.com/docs?utm_source=EmailBlasts%20&utm_medium=Open-Growth&utm_campaign=EB-CY16-Q4-Open-Growth-01&utm_term=link1&utm_content=docs-page">https://www.pubnub.com/docs</a></p>` +
+        `<p>Your API Keys` +
+        `<br />`+
+        `<a href="https://admin.pubnub.com/#/user/${user.user_id}/account/${user.account_id}/app/${user.app_id}/key/${user.key_id}/?utm_source=EmailBlasts%20&utm_medium=Open-Growth&utm_campaign=EB-CY16-Q4-Open-Growth-01&utm_term=link2&utm_content=api-keys">https://admin.pubnub.com/#/user/${user.user_id}/account/${user.account_id}/app/${user.app_id}/key/${user.key_id}/</a></p>` +
         `<p>Welcome Aboard!</p>`;
 
     // Logging Message
