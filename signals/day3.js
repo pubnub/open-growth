313729ae89ab36aa907c65bf55c41ab5039cce79
Dustin Raimondi <raimondi1337@gmail.com>
2016-12-21 19:55:42 -0800

    updated email copy

--- signals/day3.js
+++ signals/day3.js
@@ -17,14 +17,22 @@ opengrowth.signals.day3 = ( request, customer ) => {
     var fname = '';
     try       { fname = ', '+customer.person.name.givenName + '.' }
     catch (e) { fname = '.' }
+    
+    var company = '';
+    try { company = customer.company.name }
+    catch (e) { company = '' }
 
-    const message =
-        `<p>Hope you’ve had the time to navigate through the PubNub Admin Portal and the PubNub Docs Page over the last few days${fname}</p>` + //this might get weird
-        `<p>On another note, I see that you work at ${customer.company.name}. Curious if you or another member of ${customer.company.name} is assessing PubNub to power realtime capabilities in your application?</p>` + 
-        `<p>Looking forward to hearing from you soon. In the meantime, here's a developer's guide to PubNub:</p>` + 
-        `<p><a href="https://www.pubnub.com/developers/tech/how-pubnub-works/?utm_source=EmailBlasts%20&utm_medium=Open-Growth&utm_campaign=EB-CY16-Q4-Open-Growth-02&utm_term=link1&utm_content=how-pubnub-works">How PubNub Works</a></p>` +
-        `<p>Best, ${request.message.csm.first_name}</p>`+
-        `<p>This email would have been sent to: ${request.message.email}</p>`;
+    const message = `<p>Hope you’ve had the time to navigate through the PubNub Admin Portal and the PubNub Docs Page over the last few days${fname}</p>`; 
+    if (company == '') { 
+        message += `Curious if you or another member of your company is assessing PubNub to power realtime capabilities in your application?</p>`;
+    } else {
+	message += `<p>On another note, I see that you work at ${company}. Curious if you or another member of ${company} is assessing PubNub to power realtime capabilities in your application?</p>`;
+    }
+    
+    message += `<p>Looking forward to hearing from you soon. In the meantime, here's a developer's guide to PubNub:</p>` + 
+               `<p><a href="https://www.pubnub.com/developers/tech/how-pubnub-works/?utm_source=EmailBlasts%20&utm_medium=Open-Growth&utm_campaign=EB-CY16-Q4-Open-Growth-02&utm_term=link1&utm_content=how-pubnub-works">How PubNub Works</a></p>` +
+               `<p>Best, ${request.message.csm.first_name}</p>`+
+               `<p>This email would have been sent to: ${request.message.email}</p>`;
 
     opengrowth.delight.sendgrid.email(
         'day3', message, email, name, subject
