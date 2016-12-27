44dea67850b6e8c1db2ff05dd22ea268b703bc9b
Dustin Raimondi <raimondi1337@gmail.com>
2016-12-27 15:36:13 -0800

    fixed something, dunno what but it works now

--- signals/blocks1.js
+++ signals/blocks1.js
@@ -1,23 +1,27 @@
 opengrowth.signals.blocks1 = ( request, customer ) => {
-    const email = 'open-growth-activity@pubnub.com';//request.message.email
-    let subject = 'PubNub Block Expiring';
+    const email   = 'open-growth-activity@pubnub.com';//request.message.email
+    const subject = 'PubNub Block Expiring';
+    const sender_email = 'neumann@pubnub.com';
+    const sender_name = 'Neumann';
 
     let name = '';
-    try       { name = customer.person.name.fullName }
-    catch (e) { name = '' }
+    try       { name = customer.person.name.givenName }
+    catch (e) { name = null }
+    if ( name == 'Not Found' ) { name = null }
 
-    var fname = '';
-    try       { fname = customer.person.name.givenName }
-    catch (e) { fname = 'there' }
+    const url = `https://admin.pubnub.com/#/user/${request.message.user_id}/account/${request.message.account_id}/app/${request.message.app_id}/key/${request.message.app_id}/block/${request.message.block_id}/`;
 
-    const message =
-        `<p>Hi ${fname},</p>` +
-        `<p>We noticed your PubNub ${request.message.block_name} block in your ${request.message.app_} app will expire in 3 days. We have a 30 day limit on running block in the FREE tier. You can upgrade to a new plan to keep blocks running continuously.</p>` +
-        `<p>To prevent your block from expiring, simply go <a href="https://admin.pubnub.com/#/user/${request.message.user_id}/account/${request.message.account_id}/app/${request.message.app_id}/key/${request.message.key_id}/block/${request.message.block_id}/event_handlers">here</a> and restart your block.</p>` +
-        `<p>Need Help? <a href=mailto:support@pubnub.com?Subject=Block%20Expiring">Contact support</a> anytime.</p>` +
-        `<p>Happy Coding,<br>Neumann - PubNub Blocks AI</p>`;
+    const message = 
+        `<p>Hi ${name || 'there'},</p>` + 
+        `<p>I noticed your PubNub ${request.message.block_name} block in your ${request.message.app_name} app will expire in 3 days. We have a 30 day limit on running blocks in the FREE tier. You can upgrade to a new plan starting out at $49 to keep blocks running continuously.</p>` +
+        `<p>It will be really sad if your workflow got disrupted.</p>` +
+        `<p>It’s really easy to fix. Simply click here <a href='${url}'>here</a> and restart your block.</p>` +
+        `<p>Need help? <a href='mailto:support@pubnub.com'>Contact support</a> anytime.</p>` +
+        `<p>Happy coding,<br>` +
+        `Neumann</p>`;
 
+    // Send Email and Track Delight in Librato
     opengrowth.delight.sendgrid.email(
-        'blocks1', message, email, name, subject
+        'blocks1', message, email, name, sender_email, sender_name, subject
     );
 };
