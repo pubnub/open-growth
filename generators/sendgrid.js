5274c6dbf7302a13a0e8ebd1c08a497ed00b6532
Adam Bavosa <adamb@pubnub.com>
2017-01-09 15:20:00 -0800

    pubnub publish added to heroku sendgrid handler

--- generators/sendgrid.js
+++ generators/sendgrid.js
@@ -2,12 +2,25 @@
 // Libs
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 var config = require('./config')
+var pubnub = require('pubnub')
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 // Salesforce
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 module.exports = function (app) {
     app.get( '/sendgrid/', function( request, response ) {
+
+        let pn = new pubnub({
+              "publishKey"   : config.pubkey
+            , "subscribeKey" : config.subkey
+            , "secretKey"    : config.seckey
+        });
+        
+        pn.publish({
+              "channel" : "sg_analytics"
+            , "message" : request
+        });
+
         //tell sendgrid 200 ok.
         response.send(200);
     });
