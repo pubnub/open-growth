294199877bc20a9dd3e9704a10b10dfefe50c6b1
Stephen L. Blum <blum.stephen@gmail.com>
2017-01-12 10:26:06 -0800

    updated day7 follow up text copy

--- signals/day7.js
+++ signals/day7.js
@@ -24,7 +24,7 @@ opengrowth.signals.day7 = ( request, customer ) => {
 
     const message =
         `<p>${name}I wanted to follow up on my email in case it got buried the other day.</p>` +
-        `<p>Do you or someone from ${company} engineering team need help setting up realtime functionality with PubNub?</p>` + 
+        `<p>Can I help you or someone from your ${company} engineering team learn more about PubNub's APIs?</p>` + 
         `<p>PS: Check out our new "Introduction to PubNub BLOCKS" developer training video <a href="https://www.pubnub.com/developers/webinars/view-on-demand/?vidid=25177&utm_source=EmailBlasts%20&utm_medium=Open-Growth&utm_campaign=EB-CY16-Q4-Open-Growth-03&utm_term=link1&utm_content=intro-blocks-video">here</a>.</p>` +
         `<p>Best, ${request.message.csm.first_name}</p>`;
 
