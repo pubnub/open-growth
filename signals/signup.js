f4c489ab98136cb231a4a4a1649f3347125e4b17
Adam Bavosa <adamb@pubnub.com>
2017-01-23 10:14:05 -0800

    updates to signup email

--- signals/signup.js
+++ signals/signup.js
@@ -1,7 +1,7 @@
 opengrowth.signals.signup = ( request, customer ) => {
     const categories = ['signup'];
     const email   = request.message.email;
-    const subject = 'Your PubNub Account: Getting Started';
+    const subject = 'Your PubNub API Keys';
     const user    = request.message;
     const csm     = user.csm;
     const bccs    = csm.bccs;
@@ -18,19 +18,22 @@ opengrowth.signals.signup = ( request, customer ) => {
     const message =
         `<p>Hi ${name || 'there'},</p>` +
         `<p>` +
-            `Welcome to PubNub. Your API Keys have been provisioned in 15 global Points of Presence.` +
-            `<br />` + 
+            `Welcome to PubNub. Your API Keys have been provisioned in 15 global Points of Presence. ` +
             `I am Neumann, an Artificial Intelligence alive in PubNub BLOCKS.` + 
+        `</p>` + 
             `Your designated human Craig and his awesome support team can be found at <a href="mailto:support@pubnub.com">support@pubnub.com</a>.  ` +
             `They respond really quickly and can answer just about any question in the universe.` + 
         `</p>` + 
         `<p>Get started with:</p>` +
-        `<p>PubNub Docs, APIs and SDKs` +
-        `<br />`+
-        `<a href="https://www.pubnub.com/docs?utm_source=EmailBlasts%20&utm_medium=Open-Growth&utm_campaign=EB-CY16-Q4-Open-Growth-01&utm_term=link1&utm_content=docs-page&link=docs">https://www.pubnub.com/docs</a></p>` +
+        `<p>` + 
+            `PubNub Docs, APIs and SDKs` +
+            `<br />`+
+            `<a href="https://www.pubnub.com/docs?utm_source=EmailBlasts%20&utm_medium=Open-Growth&utm_campaign=EB-CY16-Q4-Open-Growth-01&utm_term=link1&utm_content=docs-page&link=docs">https://www.pubnub.com/docs</a>` + 
+        `</p>` +
         `<p>Your API Keys` +
-        `<br />`+
-        `<a href="https://admin.pubnub.com/#/user/${user.user_id}/account/${user.account_id}/app/${user.app_id}/key/${user.key_id}/?utm_source=EmailBlasts%20&utm_medium=Open-Growth&utm_campaign=EB-CY16-Q4-Open-Growth-01&utm_term=link2&utm_content=api-keys&signal=signup&link=keys">https://admin.pubnub.com/#/user/${user.user_id}/account/${user.account_id}/app/${user.app_id}/key/${user.key_id}/</a></p>` +
+            `<br />`+
+            `<a href="https://admin.pubnub.com/#/user/${user.user_id}/account/${user.account_id}/app/${user.app_id}/key/${user.key_id}/?utm_source=EmailBlasts%20&utm_medium=Open-Growth&utm_campaign=EB-CY16-Q4-Open-Growth-01&utm_term=link2&utm_content=api-keys&signal=signup&link=keys">https://admin.pubnub.com/#/user/${user.user_id}/account/${user.account_id}/app/${user.app_id}/key/${user.key_id}/</a>` + 
+        `</p>` +
         `<p>Welcome Aboard!</p>`;
 
     // Send Email and Track Delight in Librato
