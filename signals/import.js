0270ee89e65530b75a26193809b07c038a88feae
Dustin Raimondi <raimondi1337@gmail.com>
2017-01-24 10:19:54 -0800

    refactored TTL variable

--- signals/import.js
+++ signals/import.js
@@ -2,16 +2,18 @@
 //expected message format:
 //{"companyName": "email@address.com"}
 opengrowth.signals.import = (request) => {
+    const ttl = 14400; //14400 = 10 days
+
     for (var key in request.message) {
         if(key!="signal"){
             kvdb.get(key.toLowerCase().replace(/[^0-9a-z]/gi, '')).then(val => {
                 if (val) {
                     if (val.indexOf(request.message[key]) == -1) { //.includes not supported yet??
                         val.push(request.message[key]);
-                        kvdb.set(key.toLowerCase().replace(/[^0-9a-z]/gi, ''), val, 14400);
+                        kvdb.set(key.toLowerCase().replace(/[^0-9a-z]/gi, ''), val, ttl);
                     }
                 } else {
-                    kvdb.set(key.toLowerCase().replace(/[^0-9a-z]/gi, ''), [request.message[key]], 14400);
+                    kvdb.set(key.toLowerCase().replace(/[^0-9a-z]/gi, ''), [request.message[key]], ttl);
                 }
             });
         }
