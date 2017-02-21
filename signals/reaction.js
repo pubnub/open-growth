4db5622ce4599b14995c0f705c91514a3df7d470
Adam Bavosa <adamb@pubnub.com>
2017-02-21 10:09:21 -0800

    removed not ready for prod signals

--- signals/reaction.js
+++
@@ -1,6 +0,0 @@
-opengrowth.signals.reaction = ( request, customer ) => {
-    // customer param is automatically
-    // augmented with clearbit and monkeylearn
-    //console.log(customer);
-    return request.ok();
-};
