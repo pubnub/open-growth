ef98dffbca47e1bb45898a8284f8615fba753893
Adam Bavosa <adamb@pubnub.com>
2017-02-21 10:39:41 -0800

    removed log

--- signals/sendgrid_analytics.js
+++ signals/sendgrid_analytics.js
@@ -1,14 +1,12 @@
 opengrowth.signals.sendgrid_analytics = ( request ) => {
     // Tracks status of an email sent to a customer through SendGrid
     const categoryEvent = ( action ) => {
-        log(action.event, action.category, action.email);
         opengrowth.track.signal(`sendgrid_analytics.${action.category}.${action.event}`);
     }
 
     // Tracks a clicked link event in a SendGrid email
     const signalLink = ( action ) => {
         let link = getUrlParams(action.url);
-        log(action.event, action.category, action.email, link);
         opengrowth.track.signal(`sendgrid_analytics.${action.category}.link.${link}`);
     }
 
@@ -30,18 +28,6 @@ opengrowth.signals.sendgrid_analytics = ( request ) => {
         let kv = parameters.split(/&|=/);
         let link = kv[0];
         return link;
-    }
-
-    // Send all updates to logging channel
-    const log = ( event, category, email, link ) => {
-        var logObj = {
-             "action" : event
-          , "delight" : "sendgrid_analytics"
-          ,  "signal" : category
-          ,   "email" : email
-          ,    "link" : link
-        }
-        opengrowth.log(logObj);
     }
 
     var handlers = {
