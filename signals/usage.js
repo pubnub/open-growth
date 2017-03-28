ac91699a27b3a6db8b6aae4f95bc703b654c8f5b
Adam Bavosa <adamb@pubnub.com>
2017-03-27 18:15:30 -0700

    added unsubscribe link to usage email handler

---
+++ signals/usage.js
@@ -0,0 +1,40 @@
+opengrowth.signals.usage = ( request, customer ) => {
+    const user = request.message;
+    const csm  = user.csm || {};
+    const csm_bccs = csm && csm.bccs ? csm.bccs : [];
+    let email  = user.litmus || 'open-growth-activity+silver@pubnub.com';
+    // @if GOLD
+    email = user.email;
+    // @endif
+
+    let firstName    = opengrowth.customer.getFirstName(customer);
+    let lastName     = opengrowth.customer.getLastName(customer);
+    let company_name = opengrowth.customer.getCompany(customer);
+
+    var template_data = {
+        "customer_first_name" : firstName
+      , "customer_last_name"  : lastName
+      , "company_name"        : company_name
+      , "graph_url"           : user.url
+    };
+
+    let lw = opengrowth.keys.sendgrid.group.limit_warning;
+    let df = opengrowth.keys.sendgrid.group.default;
+    let fe = opengrowth.keys.sendgrid.group.feature_enable;
+
+    var sendWithUsPostBody = {
+      "template": opengrowth.keys.swu.templates.usage,
+      "recipient": {
+        "name": firstName,
+        "address": email
+      },
+      "template_data": template_data,
+      "bcc": csm_bccs,
+      "tags" : [ "og_usage" ],
+      "headers" : {
+        "x-smtpapi" : `{\"asm_group_id\":${lw},\"asm_groups_to_display\": [${lw},${df},${fe}]}`
+      }
+    };
+
+    return opengrowth.delight.sendwithus.email(sendWithUsPostBody);
+};
