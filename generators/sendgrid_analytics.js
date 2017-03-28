bfdb12a1ecb095e5a4fc50b36f11f58297e1117b
Adam Bavosa <adamb@pubnub.com>
2017-03-28 15:19:40 -0700

    sg unsub update

--- generators/sendgrid_analytics.js
+++ generators/sendgrid_analytics.js
@@ -41,7 +41,6 @@ module.exports = function ( app ) {
 
         var actions = [];
         for ( var action of request.body ) {
-            // Add unsubscribes and resubscribes to publish
             if ( action.event === "group_unsubscribe" ) {
                 action.category = [ "og_unsubscribe" ];
             }
@@ -53,7 +52,6 @@ module.exports = function ( app ) {
             if ( action.event === "unsubscribe" ) {
                 action.category = [ "og_global_unsubscribe" ];
             }
-
             //only track open growth emails that have "og_" in category
             //no tracking for default bcc
             var category = action.category || [""];
