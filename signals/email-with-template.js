b079129c6b4a959030fbb77125993bdf3cbf2022
Adam Bavosa <adamb@pubnub.com>
2017-05-11 17:58:22 -0700

    stub file for sending emails with SendWithUs template

---
+++ signals/email-with-template.js
@@ -0,0 +1,36 @@
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// Handles an email, sends with SendWithUs Template
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+opengrowth.signals.emailWithTemplate = ( request, customer ) => {
+    const message = request.message;
+    let email  = customer.email || message.email;
+    let bcc    = ""; // Set to your SalesForce tracking BCC
+
+    // Send as a template to SendWithUs with SendGrid
+    let template_data = {
+        "customer_first_name" : customer.firstName
+      , "customer_last_name"  : customer.lastName
+      , "company_name"        : customer.company
+    };
+
+    let template = user.signal;
+
+    let sendWithUsPostBody = {
+      "template": opengrowth.keys.swu.templates[template],
+      "recipient": {
+        "name": customer.firstName,
+        "address": email
+      },
+      "template_data": template_data,
+      "bcc": bcc,
+      "tags" : [ "signup" ],
+      "sender" : {
+        "name": "Neumann",
+        "address": "neumann@mycompany.com",
+        "reply_to": "support@mycompany.com"
+      }
+    };
+
+    // Send Email!
+    return opengrowth.delight.sendwithus.email(sendWithUsPostBody);
+};
